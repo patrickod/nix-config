bfb6a499856c0062032cf83cafe1bb370e51b9b9
Patrick O'Doherty <p@trickod.com>
2021-02-14 23:27:08 -0800

    Change default git branch to "main"
    remove now deprecated spacemacs git checkout

--- users/patrickod.nix
+++ users/patrickod.nix
@@ -205,7 +205,8 @@
       enable = true;
       userName = "Patrick O'Doherty";
       userEmail = "p@trickod.com";
-      extraConfig = { pull.ff = "only"; };
+      extraConfig = { pull.ff = "only";
+                      init.defaultBranch = "main"; };
     };
     programs.zsh = {
       enable = true;
@@ -262,15 +263,6 @@
       };
     };
 
-    # 2021-01-23 removed due to RO directory breaking emacs ELC compilation
-    # home.file.".emacs.d" = {
-    #   source = builtins.fetchGit {
-    #     url = "https://github.com/syl20bnr/spacemacs";
-    #     ref = "develop";
-    #     rev = "ba9e0afa34c97b310456d4352ec582d73411902b";
-    #   };
-    #   recursive = true;
-    # };
     home.file.".spacemacs".source = ../dotfiles/spacemacs;
 
     ## i3 status & keybinding configuration
