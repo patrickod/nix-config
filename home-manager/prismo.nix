412ec0b39399b6ce2012a9b634c9bbcaa817607b
Patrick O'Doherty <p@trickod.com>
2021-12-08 19:14:25 -0800

    break out kimkilwhan i3status and i3 configurations

--- home-manager/prismo.nix
+++ home-manager/prismo.nix
@@ -7,6 +7,7 @@
 
   # prismo specific i3 configuration
   xdg.configFile."i3/status.toml".source = ../dotfiles/i3status-rs.toml.prismo;
+  xdg.configFile."i3/config".source = ../dotfiles/i3-config;
 
 }
 
