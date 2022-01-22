83cf72e2859b44af57e437deef6e6bc2d4e9a12d
Patrick O'Doherty <p@trickod.com>
2022-01-21 22:54:02 -0800

    add sql to spacemacs; zoom & 1password to prismo

--- home-manager/prismo.nix
+++ home-manager/prismo.nix
@@ -5,6 +5,8 @@
 
   programs.git.extraConfig.core.editor = "emacsclient -c";
 
+  home.packages = [pkgs._1password-gui pkgs.zoom-us];
+
   # prismo specific i3 configuration
   xdg.configFile."i3/status.toml".source = ../dotfiles/i3status-rs.toml.prismo;
   xdg.configFile."i3/config".source = ../dotfiles/i3-config;
