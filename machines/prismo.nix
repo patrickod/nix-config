ef0135204ed10e9f37a2f22bb2c5360748a5908d
Patrick O'Doherty <p@trickod.com>
2022-05-30 17:29:52 -0700

    flake updates & stop noisetorch (now deleted)

--- machines/prismo.nix
+++ machines/prismo.nix
@@ -114,8 +114,6 @@
 
   virtualisation.docker.enable = true;
 
-  programs.noisetorch.enable = true;
-
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
